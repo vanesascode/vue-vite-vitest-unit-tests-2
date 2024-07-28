642d84ba4a81f6849ba56311b8630a5bbb0d7c42
vanesascode <vanesa.juarez.paris@gmail.com>
2024-07-28 15:55:07 +0200

    feat: :sparkles: add the PokemonGame View

---
+++ src/modules/pokemon/pages/PokemonGame.vue
@@ -0,0 +1,24 @@
+<template>
+  <section v-if="false">
+    <h1>Espere por favor</h1>
+    <h3>Cargando Pokémons</h3>
+  </section>
+
+  <section class="d-flex justify-center align-center flex-column">
+    <h1>¿Quién es este pokémon?</h1>
+
+    <PokemonPicture />
+
+    <PokemonOptions />
+  </section>
+</template>
+
+<script setup lang="ts">
+import PokemonPicture from '@/modules/pokemon/components/PokemonPicture.vue';
+import PokemonOptions from '@/modules/pokemon/components/PokemonOptions.vue';
+import { usePokemonGame } from '@/modules/pokemon/composables/usePokemonGame';
+
+const { gameStatus } = usePokemonGame();
+</script>
+
+<style scoped></style>
