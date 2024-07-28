4a27d1d5a51b6b17f0d17978b62f9a1041adba0d
vanesascode <vanesa.juarez.paris@gmail.com>
2024-07-28 15:49:23 +0200

    feat: :sparkles: add the vuetify config in the main.ts file

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
-import './assets/styles.css'
+import './assets/styles.css';
+// Vuetify
+import 'vuetify/styles';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
+createApp(App).use(vuetify).mount('#app');
