0e894de6e076a97434b558454d209ebccf4abb2d
vanesascode <vanesa.juarez.paris@gmail.com>
2024-07-28 15:48:43 +0200

    feat: :sparkles: import the PokemonGame component in the App.vue file

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
-  <div>
-    <h1>Hello</h1>
-  </div>
+  <main class="d-flex justify-center align-center flex-column h-screen">
+    <PokemonGame />
+  </main>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import PokemonGame from '@/modules/pokemon/pages/PokemonGame.vue';
+</script>
 
 <style scoped></style>
