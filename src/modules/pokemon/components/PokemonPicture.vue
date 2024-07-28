705d44cfd31db72523c80f98175ec735c7d35b65
vanesascode <vanesa.juarez.paris@gmail.com>
2024-07-28 15:52:52 +0200

    feat: :sparkles: add the PokemonPicture component

---
+++ src/modules/pokemon/components/PokemonPicture.vue
@@ -0,0 +1,26 @@
+<template>
+  <section>
+    <img
+      src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/132.svg"
+      alt="pokemon image"
+      class="pokemon-image"
+    />
+  </section>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.pokemon-image {
+  width: 200px;
+  height: 200px;
+  filter: brightness(0);
+
+  /* so user cannot drag image:  */
+  user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  -webkit-user-drag: none;
+  -webkit-user-select: none;
+}
+</style>
