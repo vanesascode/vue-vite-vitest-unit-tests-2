2b1e1ee8f9a725749ad10bed3255983f57301d39
vanesascode <vanesa.juarez.paris@gmail.com>
2024-07-28 15:52:29 +0200

    feat: :sparkles: add the PokemonOptions component

---
+++ src/modules/pokemon/components/PokemonOptions.vue
@@ -0,0 +1,12 @@
+<template>
+  <section class="d-flex flex-column ga-2">
+    <v-btn size="large">Pokemon 1</v-btn>
+    <v-btn size="large">Pokemon 2</v-btn>
+    <v-btn size="large">Pokemon 3</v-btn>
+    <v-btn size="large">Pokemon 4</v-btn>
+  </section>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
