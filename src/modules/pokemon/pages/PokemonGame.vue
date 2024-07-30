2438708279c23d43277f09d1d9f88323d60d4667
vanesascode <vanesa.juarez.paris@gmail.com>
2024-07-30 07:30:06 +0200

    feat: :sparkles: add the logic of the usePokemonGame composable to the PokemonPicture and the PokemonOptions components, in the PokemonGame component

--- src/modules/pokemon/pages/PokemonGame.vue
+++ src/modules/pokemon/pages/PokemonGame.vue
@@ -1,15 +1,19 @@
 <template>
-  <section v-if="false">
+  <section v-if="isLoading || randomPokemon.id === null">
     <h1>Espere por favor</h1>
     <h3>Cargando Pokémons</h3>
   </section>
 
-  <section class="d-flex justify-center align-center flex-column">
+  <section v-else class="d-flex justify-center align-center flex-column">
     <h1>¿Quién es este pokémon?</h1>
+    <h3>{{ randomPokemon }}</h3>
 
-    <PokemonPicture />
+    <PokemonPicture
+      :pokemon-id="randomPokemon.id"
+      :show-pokemon="gameStatus !== GameStatus.playing"
+    />
 
-    <PokemonOptions />
+    <PokemonOptions :options="options" @selected-option="onSelectedOption" />
   </section>
 </template>
 
@@ -17,8 +21,19 @@
 import PokemonPicture from '@/modules/pokemon/components/PokemonPicture.vue';
 import PokemonOptions from '@/modules/pokemon/components/PokemonOptions.vue';
 import { usePokemonGame } from '@/modules/pokemon/composables/usePokemonGame';
+import { GameStatus } from '../interfaces';
 
-const { gameStatus } = usePokemonGame();
+const { randomPokemon, isLoading, gameStatus, pokemonOptions: options } = usePokemonGame();
+
+const onSelectedOption = (id: number) => {
+  if (gameStatus.value === GameStatus.playing) {
+    if (id === randomPokemon.value.id) {
+      gameStatus.value = GameStatus.won;
+    } else {
+      gameStatus.value = GameStatus.lost;
+    }
+  }
+};
 </script>
 
 <style scoped></style>
