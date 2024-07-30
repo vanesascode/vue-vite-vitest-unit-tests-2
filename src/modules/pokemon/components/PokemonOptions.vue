d7773684ff38d353686acadd67ab66fd9715b0d0
vanesascode <vanesa.juarez.paris@gmail.com>
2024-07-30 07:26:30 +0200

    feat: :sparkles: add the logic to the buttons that have the pokemon options for the player, in the PokemonOptions component

--- src/modules/pokemon/components/PokemonOptions.vue
+++ src/modules/pokemon/components/PokemonOptions.vue
@@ -1,12 +1,27 @@
 <template>
-  <section class="d-flex flex-column ga-2">
-    <v-btn size="large">Pokemon 1</v-btn>
-    <v-btn size="large">Pokemon 2</v-btn>
-    <v-btn size="large">Pokemon 3</v-btn>
-    <v-btn size="large">Pokemon 4</v-btn>
+  <section class="d-flex flex-column ga-3 mt-3">
+    <v-btn
+      v-for="{ name, id } in options"
+      :key="id"
+      size="large"
+      @click="$emit('selectedOption', id)"
+      >{{ name }}</v-btn
+    >
   </section>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type { Pokemon } from '../interfaces';
+
+interface PokemonOptionsProps {
+  options: Pokemon[];
+}
+
+defineProps<PokemonOptionsProps>();
+
+defineEmits<{
+  selectedOption: [id: number];
+}>();
+</script>
 
 <style scoped></style>
