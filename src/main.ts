d7f6bdb23a10ca104eab273f13f4d638cc661d50
vanesascode <vanesa.juarez.paris@gmail.com>
2024-08-04 13:23:57 +0200

    feat: :sparkles: add the pinia store to the main.ts file

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import './assets/styles.css';
 import './assets/animations.css';
 
-// Vuetify
 import 'vuetify/styles';
 import { createVuetify } from 'vuetify';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
 
+import { createPinia } from 'pinia';
 import { createApp } from 'vue';
+
 import App from './App.vue';
 
 const vuetify = createVuetify({
@@ -15,4 +16,6 @@ const vuetify = createVuetify({
   directives,
 });
 
-createApp(App).use(vuetify).mount('#app');
+const pinia = createPinia();
+
+createApp(App).use(vuetify).use(pinia).mount('#app');
