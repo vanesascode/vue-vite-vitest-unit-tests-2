a5c03c3ff6eb83dd8fdf2bb5d570a8bc06e3c7fa
vanesascode <vanesa.juarez.paris@gmail.com>
2024-08-04 13:27:09 +0200

    feat: :sparkles: add components and logic to the PokemonGame View

--- src/modules/pokemon/pages/PokemonGame.vue
+++ src/modules/pokemon/pages/PokemonGame.vue
@@ -5,35 +5,83 @@
   </section>
 
   <section v-else class="d-flex justify-center align-center flex-column">
-    <h1>¿Quién es este pokémon?</h1>
-    <h3>{{ randomPokemon }}</h3>
+    <h1 v-if="gameStatus === GameStatus.playing">¿Quién es este pokémon?</h1>
+    <div style="height: 3rem" v-else>
+      <h1 v-if="gameStatus === GameStatus.won" class="text-capitalize">
+        ¡Correcto! ¡Es {{ randomPokemon.name }}!
+      </h1>
+
+      <h1 v-else-if="gameStatus === GameStatus.lost" class="text-capitalize">
+        Perdiste... Es {{ randomPokemon.name }}
+      </h1>
+    </div>
+
+    <div style="height: 3rem">
+      <v-btn
+        v-if="gameStatus !== GameStatus.playing"
+        @click="getNextRound()"
+        class="text-capitalize w-100"
+        style="min-width: 10.5rem"
+        >Juega otra vez
+      </v-btn>
+    </div>
 
     <PokemonPicture
       :pokemon-id="randomPokemon.id"
       :show-pokemon="gameStatus !== GameStatus.playing"
     />
 
-    <PokemonOptions :options="options" @selected-option="onSelectedOption" />
+    <PokemonOptions
+      :options="options"
+      @selected-option="checkAnswer"
+      :block-options="gameStatus !== GameStatus.playing"
+      :correct-answer="randomPokemon.id"
+    />
+    <div class="d-flex justify-center align-start ga-10 flex-wrap mt-10 mb-3 px-4">
+      <PointsBox :wins="wins" :loses="loses" :times-played="timesPlayed" />
+      <GameLevels @options-number="changeLevel" />
+    </div>
   </section>
 </template>
 
 <script setup lang="ts">
-import PokemonPicture from '@/modules/pokemon/components/PokemonPicture.vue';
-import PokemonOptions from '@/modules/pokemon/components/PokemonOptions.vue';
-import { usePokemonGame } from '@/modules/pokemon/composables/usePokemonGame';
+import { computed, watch } from 'vue';
+import { useLevelStore } from '@/stores/useLevelStore';
+import { usePointsStore } from '@/stores/usePointsStore';
+import PokemonPicture from '@pokemon/components/PokemonPicture.vue';
+import PokemonOptions from '@pokemon/components/PokemonOptions.vue';
+import GameLevels from '@pokemon/components/GameLevels.vue';
+import { usePokemonGame } from '@pokemon/composables/usePokemonGame';
 import { GameStatus } from '../interfaces';
+import PointsBox from '../components/PointsBox.vue';
 
-const { randomPokemon, isLoading, gameStatus, pokemonOptions: options } = usePokemonGame();
+const levelStore = useLevelStore();
+const pointsStore = usePointsStore();
 
-const onSelectedOption = (id: number) => {
-  if (gameStatus.value === GameStatus.playing) {
-    if (id === randomPokemon.value.id) {
-      gameStatus.value = GameStatus.won;
-    } else {
-      gameStatus.value = GameStatus.lost;
-    }
-  }
+const wins = computed(() => pointsStore.wins);
+const loses = computed(() => pointsStore.loses);
+const timesPlayed = computed(() => pointsStore.timesPlayed);
+
+const changeLevel = (level: number) => {
+  levelStore.setLevel(level);
+  getNextRound(level);
 };
+const {
+  randomPokemon,
+  isLoading,
+  gameStatus,
+  pokemonOptions: options,
+  checkAnswer,
+  getNextRound,
+} = usePokemonGame();
+
+watch(gameStatus, (newStatus) => {
+  if (newStatus === GameStatus.won) {
+    pointsStore.incrementWins();
+  } else if (newStatus === GameStatus.lost) {
+    pointsStore.incrementLoses();
+  }
+});
 </script>
 
 <style scoped></style>
